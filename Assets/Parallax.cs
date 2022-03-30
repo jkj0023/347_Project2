53bf295952e94a05221376f388995995724d2d67
jkj0023 <90661761+jkj0023@users.noreply.github.com>
2022-03-30 17:02:24 -0500

    First Commit, menu screen transitions to game scene when button is pressed

---
+++ Assets/Parallax.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class Parallax : MonoBehaviour
+{
+    float scrollSpeed = -5f;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float newPos = Mathf.Repeat(20, Time.time * scrollSpeed);
+        transform.position = startPos + Vector2.up * newPos;
+}*/
